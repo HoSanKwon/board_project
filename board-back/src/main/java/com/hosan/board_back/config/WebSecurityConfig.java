6822f7d152009c6924643e82310c6e2e8b0bb138
kwonhosan <rnjs5787@naver.com>
2024-08-14 01:12:25 +0900

    add config

---
+++ board-back/src/main/java/com/hosan/board_back/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.hosan.board_back.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.hosan.board_back.filter.JwtAuthenticationFilter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors().and() // CORS 설정
+            .csrf().disable() // CSRF 비활성화
+            .httpBasic().disable() // HTTP Basic 인증 비활성화
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and() // 세션 관리 설정
+            .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll() // 특정 경로 허용
+            .anyRequest().authenticated() // 그 외 모든 요청 인증 필요
+            );
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
+
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{ \"code\": \"NP\", \"message\": \"Do not have permission.\"}");
+
+    }
+
+}

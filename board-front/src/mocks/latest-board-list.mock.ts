566aedcf4ab91fd9c6f16206a459d870330c1f0f
kwonhosan <rnjs5787@naver.com>
2024-08-19 02:22:01 +0900

    front - add board view

---
+++ board-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,54 @@
+import { BoardListItem } from 'types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐 먹지.. 일반식 먹을까 식단 할까?",
+        content: "음.. 푸아그라, 북경오리, 탕수육, 제육볶음, 샐러드, 햄버거, 피자, 닭가슴살이나 먹자..!",
+        boardTitleImage: 'https://img.choroc.com/newshop/goods/019011/019011_1.jpg',
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2024.08.15 02:13:42",
+        writerNickname: "안녕하세요나는호산",
+        writerProfileImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdru7ecyO9X3QJWqe0_2ZGS63uHPfor6rSJw&s'
+    },
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐 먹지.. 일반식 먹을까 식단 할까?",
+        content: "음.. 푸아그라, 북경오리, 탕수육, 제육볶음, 샐러드, 햄버거, 피자, 닭가슴살이나 먹자..!",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2024.08.15 02:13:42",
+        writerNickname: "안녕하세요나는싸딕",
+        writerProfileImage: 'https://mblogthumb-phinf.pstatic.net/MjAyMjA1MDhfMjA5/MDAxNjUyMDA4NjQxODQz.ijXpggLmEw0C36RkNT4XD2GllJ0yvpE6zItDOny8LbYg.VmNJq6tJPkRxUmIKz3wdnmbnXr0w9zLZw9vlIv0Z-sEg.JPEG.gymgora/SE-4a183ec2-5ca4-4143-a28d-ec81761bb124.jpg?type=w800'
+    },
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐 먹지.. 일반식 먹을까 식단 할까?",
+        content: "음.. 푸아그라, 북경오리, 탕수육, 제육볶음, 샐러드, 햄버거, 피자, 닭가슴살이나 먹자..!",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2024.08.15 02:13:42",
+        writerNickname: "안녕하세요나는호산",
+        writerProfileImage: null
+    },
+    {
+        boardNumber: 1,
+        title: "오늘 점심 뭐 먹지.. 일반식 먹을까 식단 할까?",
+        content: "음.. 푸아그라, 북경오리, 탕수육, 제육볶음, 샐러드, 햄버거, 피자, 닭가슴살이나 먹자..!",
+        boardTitleImage: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        viewCount: 0,
+        writeDatetime: "2024.08.15 02:13:42",
+        writerNickname: "안녕하세요나는호산",
+        writerProfileImage: null
+    },
+]
+
+export default latestBoardListMock;
--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -6,5 +6,4 @@ export type {
     BoardListItem,
     CommentListItem,
     FavoriteListItem
-    
 }
